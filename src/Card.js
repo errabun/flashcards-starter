715535611802c158aa37377fa83bb34ee98fea20
Eric Rabun <errabun@gmail.com>
2021-03-22 18:15:16 -0600

    Create Card class and constructor

---
+++ src/Card.js
@@ -0,0 +1,10 @@
+class Card {
+  constructor(id, question, answers, correctAnswer) {
+    this.id;
+    this.question;
+    this.answers = [];
+    this.correctAnswer; 
+  }
+}
+
+module.exports = Card;
