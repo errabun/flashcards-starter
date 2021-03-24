b533588dbfa426149f64545e1e2d5a212cf82bf1
Eric Rabun <errabun@gmail.com>
2021-03-23 19:34:36 -0600

    Complete all Deck-test.js and Deck.js files

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Deck = require('../src/Deck');
-const Card = require('../src/Card'); 
+const Card = require('../src/Card');
 
 describe('Deck', function() {
 
@@ -17,3 +17,27 @@ describe('Deck', function() {
 
     expect(deck).to.be.an.instanceof(Deck);
   });
+
+  it('should be initialized with an array of Cards', function() {
+    const card1 = new Card (10, 'Which iteration method returns the first array element where the callback function returns true', ['find()', 'filter()', 'forEach()'], 'find()');
+    const card2 = new Card (1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card3 = new Card (7, 'What data type is wrapped in quoties?', ['string', 'array', 'function'], 'string');
+
+    const cards = [card1, card2, card3]
+    const deck = new Deck(cards);
+
+    expect(cards).to.be.a('array');
+    expect(card2.correctAnswer).to.equal('object');
+  })
+
+  it('should be able to see how many cards are in the deck', function() {
+    const card1 = new Card (10, 'Which iteration method returns the first array element where the callback function returns true', ['find()', 'filter()', 'forEach()'], 'find()');
+    const card2 = new Card (1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    const card3 = new Card (7, 'What data type is wrapped in quoties?', ['string', 'array', 'function'], 'string');
+
+    const cards = [card1, card2, card3]
+    const deck = new Deck(cards);
+
+    expect(deck.countCards()).to.equal(3);
+  })
+})
